d2e1bac95ba191e06fda2f80ae38fbd23ef1d6b3
Lawrence Corso <lawrencecorso1@gmail.com>
2026-04-23 14:50:21 -0500

    LOVE

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
         <div className="max-w-7xl mx-auto px-6 h-20 flex items-center justify-between">
           <div className="flex items-center gap-4">
             <div className="flex items-center gap-3">
-              <img src="/j.svg" alt="Logo" className="w-8 h-8 object-contain" />
+              <img src="/r.svg" alt="Logo" className="w-8 h-8 object-contain" />
               <div className="text-xl font-black tracking-tighter text-white uppercase italic">Vitae</div>
             </div>
             <div className="h-4 w-[1px] bg-white/10" />
